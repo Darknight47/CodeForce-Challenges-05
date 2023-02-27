3afbe1e449bdce23b2679b604cf56ec37ec9b669
DarkNight <farhadgunners@gmail.com>
2023-02-27 22:56:04 +0100

    math

---
+++ Quantifiers.py
@@ -0,0 +1,19 @@
+"""
+-------- Link for the challenge: https://codeforces.com/contest/952/problem/A --------
+Input
+The input contains a single integer a (10 ≤ a ≤ 999).
+
+Output
+Output 0 or 1.
+
+Input:
+10
+Output:
+0
+
+"""
+a = int(input())
+if(a % 2 == 0):
+    print(0)
+else:
+    print(1)
